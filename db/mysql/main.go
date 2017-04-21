50e4ea20e3b5a0e4aeec396c59767e3766584038
howardliu-cn <liuxinghao1988@gmail.com>
2017-04-21 10:03:22 +0800

    sql

---
+++ db/mysql/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go_db?charset=utf8")
+	checkErr(err)
+
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	checkErr(err)
+
+	defer db.Close()
+	defer stmt.Close()
+
+	res, err := stmt.Exec("u1", "d1", "2017-04-20")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("u2", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+	fmt.Println(affect)
+
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err := rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid, username, department, created)
+	}
+
+	stmt,err=db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res,err=stmt.Exec(id)
+	checkErr(err)
+
+	affect,err=res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+}
+
---
+++ db/sqlite/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "db/sqlite/test.db")
+	checkErr(err)
+
+	stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) VALUES (?, ?, ?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("u1", "p1", time.Now().Format("2006-01-02 15:04:05"))
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+
+	stmt, err = db.Prepare("UPDATE userinfo SET username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("u2", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	rows, err := db.Query("SELECT * FROM userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created string
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println(uid, username, department, created)
+	}
+
+	stmt, err = db.Prepare("DELETE FROM userinfo WHERE uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	defer db.Close()
+	defer stmt.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
