f6e1196e922b3a64ae4f23e9151edc9bead9e195
howardliu-cn <liuxinghao1988@gmail.com>
2017-04-21 10:03:07 +0800

    web service

---
+++ web-server/form/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("schema", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("val: ", strings.Join(v, " "))
+	}
+	fmt.Fprintf(w, "Hello, World!\n")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("web-server/form/login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		r.ParseForm()
+		fmt.Println("username: ", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password: ", r.Form["password"])
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
